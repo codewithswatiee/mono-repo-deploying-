ebd2dede77cf12cfe6bee6e79bb1e14e2afce2d2
codewithswatiee <verma.s@somaiya.edu>
2025-08-03 20:56:55 +0530

    feat(config): update Next.js configuration and modify dev script in package.json

--- my-turborepo/apps/web/next.config.js
+++ my-turborepo/apps/web/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+    turbo: {
+      resolveExtensions: ['.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+    },
+  },
+  transpilePackages: ['@repo/ui', '@repo/db'],
+};
 
 export default nextConfig;
